9d004c42dac68adf8e1563bb41e3122938aa7d59
Philly_Chien <Philly_Chien@cmoney.com.tw>
2021-10-19 20:47:07 +0800

    added Dockerfile

--- src/infra/sqsConsumer/config/config.ts
+++ src/infra/sqsConsumer/config/config.ts
@@ -1,5 +1,5 @@
 import * as dotenv from "dotenv";
-import {DevSqsConfig} from "./interfaces/SqsConfig";
+import {DevSqsConfig, SqsConfig} from "./interfaces/SqsConfig";
 dotenv.config();
 
 const sqsConfig: DevSqsConfig = {
@@ -9,12 +9,26 @@ const sqsConfig: DevSqsConfig = {
         region: process.env.DEV_ORDERING_PROCESS_REGION
     },
     test:{
-
+        queueName: process.env.TEST_QUEUE_NAME,
+        queueUrl: process.env.TEST_ORDERING_PROCESS_QUEUE_URL,
+        region: process.env.TEST_ORDERING_PROCESS_REGION
     },
     production:{
-
+        queueName: process.env.PROD_QUEUE_NAME,
+        queueUrl: process.env.PROD_ORDERING_PROCESS_QUEUE_URL,
+        region: process.env.PROD_ORDERING_PROCESS_REGION
     }
 
 }
+
+function checkConfigSetting(config: SqsConfig){
+    if(!config.queueUrl || !config.queueName || !config.region){
+        const errorMessage =`SQS config not completed!`;
+        console.error(errorMessage);
+        throw new Error(errorMessage);
+    }
+}
+
+checkConfigSetting(sqsConfig[process.env.NODE_ENV])
 
 export default sqsConfig
